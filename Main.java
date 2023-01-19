5c7fa3d7593d6241a2ce2d8e5b545b08d4d9c0bd
inneryou <inna.fullback@gmail.com>
2023-01-19 23:45:30 +0900

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,9 @@
+public class Main{
+  public static void main(String[] args){
+    BrowserLauncher lp = new BrowserLauncher();
+    lp.setTitle("Luncheeer Panel");
+    lp.setBounds(200,200,200,200);
+    lp.setSize(300,200);
+    lp.setShow();
+  }
+}
