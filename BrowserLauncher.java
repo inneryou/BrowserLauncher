b2f4e390406c6c788a75476b28c9bb29d280187e
inneryou <inna.fullback@gmail.com>
2023-01-21 03:23:51 +0900

    Update BrowserLauncher.java

--- BrowserLauncher.java
+++ BrowserLauncher.java
@@ -1,27 +1,58 @@
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
+
 import java.awt.FlowLayout;
 import java.awt.event.ActionListener;
 import java.awt.Desktop;
 import java.awt.event.*;
 import java.net.URL;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class BrowserLauncher extends JPanel implements ActionListener{
-  
-  JFrame mainFrame = new JFrame();
-  JButton launchButton = new JButton("Launch Browser");
-  JPanel mainPanel = new JPanel();
-  
+  private JFrame mainFrame;
+  private JLabel clockdispay;
+  private AlarmClock alarmclock;
+  private JLabel alarmTime;
+  private JButton launchButton;
+  private JButton setAlarmButton;
+  private JTextField inputTime;
+
   public BrowserLauncher(){
+    JPanel mainPanel = new JPanel();
+    JPanel clockPanel = new JPanel();
+    launchButton = new JButton("Launch Browser");
+    setAlarmButton = new JButton("アラーム");
+    inputTime = new JTextField(10);
+    alarmTime = new JLabel();
+    clockdispay = new JLabel();
+    mainFrame = new JFrame();
+    alarmclock = new AlarmClock();
     mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     launchButton.addActionListener(this);
+    setAlarmButton.addActionListener(this);
     mainPanel.setLayout(new FlowLayout());
     mainPanel.add(launchButton);
+    clockPanel.add(clockdispay);
+    clockPanel.add(inputTime);
+    clockPanel.add(alarmTime);
+    clockPanel.add(setAlarmButton);
     mainFrame.getContentPane().add(mainPanel);
+    mainPanel.add(clockPanel);
     mainFrame.setAlwaysOnTop(true);
   }
   
+  public void start(){
+    Timer timer = new Timer();
+    timer.scheduleAtFixedRate(new TimerTask(){
+      public void run(){
+        clockdispay.setText(alarmclock.getTimeStr());
+      }
+    },0,1000);
+  }
   public void setShow(){
     mainFrame.setVisible(true);
   }
@@ -39,8 +70,18 @@ public class BrowserLauncher extends JPanel implements ActionListener{
   }
 
   public void actionPerformed(ActionEvent e){
-    openURL("http://turedurenaru.com");
+    Object obj = e.getSource();
+    if(obj == launchButton){
+      openURL("http://turedurenaru.com");
+    }else if(obj == setAlarmButton){
+      alarmTime.setText(alarmclock.getAlarmTimeStr(Integer.parseInt(inputTime.getText())));
+    }
   }
+
+  public void showTime(String str){
+    clockdispay.setText(str);
+  }
+
   public void openURL(String strUrl){
     try{
       URL url = new URL(strUrl);
